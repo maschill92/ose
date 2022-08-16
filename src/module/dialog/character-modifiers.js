27dbfe769f90760e219b9b7f5856224f3d508c6c
Michael Schilling <maschill92@gmail.com>
2022-08-16 16:19:59 -0500

    Typecheck character-modifiers

--- src/module/dialog/character-modifiers.js
+++ src/module/dialog/character-modifiers.js
@@ -1,3 +1,4 @@
+//@ts-check
 import { OSE } from "../config";
 
 export class OseCharacterModifiers extends FormApplication {
@@ -17,6 +18,7 @@ export class OseCharacterModifiers extends FormApplication {
    * @type {String}
    */
   get title() {
+    // @ts-ignore
     return `${this.object.name}: Modifiers`;
   }
 
@@ -26,7 +28,9 @@ export class OseCharacterModifiers extends FormApplication {
    * Construct and return the data object used to render the HTML template for this form application.
    * @return {Object}
    */
+  // @ts-ignore
   getData() {
+    // @ts-ignore
     const data = foundry.utils.deepClone(this.object.data);
     data.user = game.user;
     return data;
@@ -34,8 +38,20 @@ export class OseCharacterModifiers extends FormApplication {
 
   /* -------------------------------------------- */
 
-  /** @override */
+  /**
+   * @override
+   * @param {JQuery} html
+   */
   activateListeners(html) {
     super.activateListeners(html);
+  }
+
+  /**
+   * Overriding as it's an abstract function on FormApplication. Perhaps this class should extend Application instead?
+   * @override
+   * @returns {Promise<unknown>}
+   */
+  _updateObject() {
+    return Promise.resolve();
   }
 }
