3993df246234c76740ef897d34d1362db01c09a9
Michael Schilling <maschill92@gmail.com>
2022-08-16 15:51:19 -0500

    Type check party-xp

--- src/module/party/party-xp.js
+++ src/module/party/party-xp.js
@@ -1,5 +1,7 @@
+//@ts-check
 import { OseParty } from "./party";
 import { OSE } from "../config";
+import { OseActor } from "../actor/entity";
 
 export class OsePartyXP extends FormApplication {
   static get defaultOptions() {
@@ -27,24 +29,33 @@ export class OsePartyXP extends FormApplication {
 
   /**
    * Construct and return the data object used to render the HTML template for this form application.
-   * @return {Object}
+   * @return {{actors: OseActor[], data: {}, config: import("../config").OseConfig, user: User | null, settings: any}}
    */
+  // @ts-ignore
   getData() {
     let data = {
       actors: OseParty.currentParty,
       data: this.object,
       config: CONFIG.OSE,
       user: game.user,
+      // TODO: remove settings? copy/paste from party-sheet?
+      //@ts-ignore
       settings: settings,
     };
     return data;
   }
 
+  /**
+   *
+   * @param {DragEvent} event
+   * @returns
+   */
   _onDrop(event) {
     event.preventDefault();
     // WIP Drop Item Quantity
     let data;
     try {
+      // @ts-ignore
       data = JSON.parse(event.dataTransfer.getData("text/plain"));
       if (data.type !== "Item") return;
     } catch (err) {
@@ -53,39 +64,72 @@ export class OsePartyXP extends FormApplication {
   }
   /* -------------------------------------------- */
 
-  _updateObject(event, formData) {
+  /**
+   *
+   * @param {Event} event
+   * @param {never} formData
+   */
+  async _updateObject(event, formData) {
     this._dealXP(event);
   }
 
+  /**
+   *
+   * @param {Event} ev
+   */
   _calculateShare(ev) {
     const currentParty = OseParty.currentParty;
 
+    // @ts-ignore
     const html = $(this.form);
+    // @ts-ignore
     const totalXP = html.find('input[name="total"]').val();
     const baseXpShare = parseFloat(totalXP) / currentParty.length;
 
     currentParty.forEach((a) => {
+      if (a.data.type !== "character") {
+        return;
+      }
       const xpShare = Math.floor(
         (a.data.data.details.xp.share / 100) * baseXpShare
       );
+      // @ts-ignore
       html.find(`li[data-actor-id='${a.id}'] input`).val(xpShare);
     });
   }
 
+  /**
+   *
+   * @param {Event} ev
+   */
   _dealXP(ev) {
+    // @ts-ignore
     const html = $(this.form);
+    // @ts-ignore
     const rows = html.find(".actor");
-    rows.each((_, row) => {
-      const qRow = $(row);
-      const value = qRow.find("input").val();
-      const id = qRow.data("actorId");
-      const actor = OseParty.currentParty.find((e) => e.id === id);
-      if (value) {
-        actor.getExperience(Math.floor(parseInt(value)));
+    rows.each(
+      /**
+       *
+       * @param {number} _
+       * @param {HTMLElement} row
+       */
+      (_, row) => {
+        const qRow = $(row);
+        const value = qRow.find("input").val();
+        const id = qRow.data("actorId");
+        const actor = OseParty.currentParty.find((e) => e.id === id);
+        if (value && actor) {
+          // @ts-ignore
+          actor.getExperience(Math.floor(parseInt(value)));
+        }
       }
-    });
+    );
   }
 
+  /**
+   *
+   * @param {JQuery} html
+   */
   activateListeners(html) {
     super.activateListeners(html);
 
@@ -93,6 +137,7 @@ export class OsePartyXP extends FormApplication {
     totalField.on("input", this._calculateShare.bind(this));
 
     html.find('button[data-action="deal-xp"').click((event) => {
+      // @ts-ignore
       super.submit(event);
     });
   }
