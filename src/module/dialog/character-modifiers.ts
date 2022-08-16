894512b595ec708f826bfb714d2f9839ab6c4811
Michael Schilling <maschill92@gmail.com>
2022-08-16 18:41:03 -0500

    Typescript-ify character-modifiers

--- src/module/dialog/character-modifiers.ts
+++ src/module/dialog/character-modifiers.ts
@@ -1,7 +1,16 @@
-//@ts-check
+import { OseActor } from "../actor/entity";
 import { OSE } from "../config";
 
-export class OseCharacterModifiers extends FormApplication {
+interface OseCharacterCreatorOptions extends FormApplicationOptions {}
+interface OseCharacterCreatorData
+extends FormApplication.Data<OseActor, OseCharacterCreatorOptions> {
+  user: User;
+}
+// TODO: Can this extend Application?
+export class OseCharacterModifiers extends FormApplication<
+  OseCharacterCreatorOptions,
+  OseCharacterCreatorData
+> {
   static get defaultOptions() {
     const options = super.defaultOptions;
     (options.classes = ["ose", "dialog", "modifiers"]),
@@ -26,32 +35,27 @@ export class OseCharacterModifiers extends FormApplication {
 
   /**
    * Construct and return the data object used to render the HTML template for this form application.
-   * @return {Object}
    */
-  // @ts-ignore
-  getData() {
-    // @ts-ignore
+  getData(): OseCharacterCreatorData {
     const data = foundry.utils.deepClone(this.object.data);
+    // TODO: This isn't used in the HTML file anywhere. Remove?
+    //@ts-ignore
     data.user = game.user;
+    //@ts-ignore
     return data;
   }
 
   /* -------------------------------------------- */
 
-  /**
-   * @override
-   * @param {JQuery} html
-   */
-  activateListeners(html) {
+  /** @override */
+  activateListeners(html: JQuery) {
     super.activateListeners(html);
   }
 
   /**
    * Overriding as it's an abstract function on FormApplication. Perhaps this class should extend Application instead?
-   * @override
-   * @returns {Promise<unknown>}
    */
-  _updateObject() {
+  override _updateObject(): Promise<unknown> {
     return Promise.resolve();
   }
 }
