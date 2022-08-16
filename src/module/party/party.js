6435aecaff9c0f6024498cebd6dd9d9a4ee6bfa3
Michael Schilling <maschill92@gmail.com>
2022-08-16 16:13:52 -0500

    Typecheck party.js

--- src/module/party/party.js
+++ src/module/party/party.js
@@ -1,11 +1,18 @@
+//@ts-check
+import { OseActor } from "../actor/entity";
+
 export class OseParty {
+  /**
+   * @returns {OseActor[]}
+   */
   static get currentParty() {
-    const characters = game.actors.filter(
+    const characters = game.actors?.filter(
       (act) =>
         act.data.type === "character" &&
         act.data.flags.ose &&
-        act.data.flags.ose.party === true);
+        act.data.flags.ose.party === true
+    );
 
-    return characters;
+    return characters || [];
   }
 }
