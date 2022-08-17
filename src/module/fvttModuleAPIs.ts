195f7162cefe0c0371e322e133295ca0675eaaa1
Michael Schilling <maschill92@gmail.com>
2022-08-17 16:30:16 -0500

    Typescript-ify fvttModuleAPIs

--- src/module/fvttModuleAPIs.ts
+++ src/module/fvttModuleAPIs.ts
@@ -6,6 +6,7 @@ export const registerFVTTModuleAPIs = () => {
     if (ItemPiles.API.ITEM_QUANTITY_ATTRIBUTE !== "data.quantity.value")
       ItemPiles.API.setItemQuantityAttribute("data.quantity.value");
     if (
+      // FIXME: This if statement will always evaluate to true, as it's a new instance of the array
       ItemPiles.API.ITEM_FILTERS !==
       [
         {
@@ -22,3 +23,17 @@ export const registerFVTTModuleAPIs = () => {
       ]);
   });
 };
+
+// only declare ItemPiles as global for this module so it doesn't pollute the global namespace
+declare var ItemPiles: ItemPiles;
+
+interface ItemPiles {
+  API: {
+    ACTOR_CLASS_TYPE: string;
+    setActorClassType: (arg0: string) => void;
+    ITEM_QUANTITY_ATTRIBUTE: string;
+    setItemQuantityAttribute: (arg0: string) => void;
+    ITEM_FILTERS: { path: string; filters: string }[];
+    setItemFilters: (arg0: { path: string; filters: string }[]) => void;
+  };
+}
