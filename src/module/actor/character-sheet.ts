d99aed7169d5f864e1f4d48182348078047d0a36
Michael Schilling <maschill92@gmail.com>
2022-08-18 15:28:55 -0500

    Typescript-ify character-sheet

--- src/module/actor/character-sheet.ts
+++ src/module/actor/character-sheet.ts
@@ -1,23 +1,16 @@
-// @ts-nocheck
 import { OseActorSheet } from "./actor-sheet";
 import { OseCharacterModifiers } from "../dialog/character-modifiers";
 import { OseCharacterGpCost } from "../dialog/character-gp-cost.js";
 import { OseCharacterCreator } from "../dialog/character-creation";
-import { OSE } from "../config";
+import { Attribute, ExplorationSkill, OSE } from "../config";
+import { OseItem } from "../item/entity";
 
 /**
  * Extend the basic ActorSheet with some very simple modifications
  */
 export class OseActorSheetCharacter extends OseActorSheet {
-  constructor(...args) {
-    super(...args);
-  }
-
-  /* -------------------------------------------- */
-
   /**
    * Extend and override the default options used by the 5e Actor Sheet
-   * @returns {Object}
    */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
@@ -41,22 +34,35 @@ export class OseActorSheetCharacter extends OseActorSheet {
    * Organize and classify Owned Items for Character sheets
    * @private
    */
-  _prepareItems(data) {
+  _prepareItems(data: any) {
     const itemsData = this.actor.data.items;
-    const containerContents = {};
+    const containerContents: { [key: string]: OseItem[] } = {};
     // Partition items by category
     let [containers, treasures, items, weapons, armors, abilities, spells] =
-      itemsData.reduce(
+      itemsData.reduce<
+        [
+          OseItem[],
+          OseItem[],
+          OseItem[],
+          OseItem[],
+          OseItem[],
+          OseItem[],
+          OseItem[]
+        ]
+      >(
         (arr, item) => {
           // Classify items into types
-          const containerId = item?.data?.data?.containerId;
+          const containerId =
+            "containerId" in item?.data?.data
+              ? item.data.data.containerId
+              : null;
           if (containerId) {
             containerContents[containerId] = [
               ...(containerContents[containerId] || []),
               item,
             ];
           } else if (item.type === "container") arr[0].push(item);
-          else if (item.type === "item" && item?.data?.data?.treasure)
+          else if (item.type === "item" && "treasure" in item?.data?.data)
             arr[1].push(item);
           else if (item.type === "item") arr[2].push(item);
           else if (item.type === "weapon") arr[3].push(item);
@@ -68,29 +74,37 @@ export class OseActorSheetCharacter extends OseActorSheet {
         [[], [], [], [], [], [], []]
       );
     // Sort spells by level
-    var sortedSpells = {};
-    var slots = {};
+    var sortedSpells: { [key: number]: OseItem[] } = {};
+    var slots: { [key: number]: number } = {};
     for (var i = 0; i < spells.length; i++) {
-      const lvl = spells[i].data.data.lvl;
+      const spell = spells[i];
+      if (spell.data.type !== "spell") {
+        continue;
+      }
+      const lvl = spell.data.data.lvl;
       if (!sortedSpells[lvl]) sortedSpells[lvl] = [];
       if (!slots[lvl]) slots[lvl] = 0;
-      slots[lvl] += spells[i].data.data.memorized;
-      sortedSpells[lvl].push(spells[i]);
+      slots[lvl] += spell.data.data.memorized;
+      sortedSpells[lvl].push(spell);
     }
     data.slots = {
       used: slots,
     };
-    containers.map((container, key, arr) => {
-      arr[key].data.data.itemIds = containerContents[container.id] || [];
-      arr[key].data.data.totalWeight = containerContents[container.id]?.reduce(
-        (acc, item) => {
-          return (
-            acc +
-            item.data?.data?.weight * (item.data?.data?.quantity?.value || 1)
-          );
-        },
-        0
-      );
+    containers.forEach((container, key, arr) => {
+      if (container.data.type !== "container") {
+        return;
+      }
+      container.data.data.itemIds = containerContents[container.id!] || [];
+      // @ts-ignore add this to derived data?
+      container.data.data.totalWeight = containerContents[
+        container.id!
+      ]?.reduce((acc, item) => {
+        return (
+          acc +
+          // @ts-ignore should to some item.data.type checking here?
+          item.data?.data?.weight * (item.data?.data?.quantity?.value || 1)
+        );
+      }, 0);
       return arr;
     });
 
@@ -111,12 +125,15 @@ export class OseActorSheetCharacter extends OseActorSheet {
       ...Object.values(data.owned),
       ...Object.values(data.spells),
       data.abilities,
+      // @ts-ignore
     ].forEach((o) => o.sort((a, b) => (a.data.sort || 0) - (b.data.sort || 0)));
   }
 
   generateScores() {
     new OseCharacterCreator(this.actor, {
+      // @ts-ignore
       top: this.position.top + 40,
+      // @ts-ignore
       left: this.position.left + (this.position.width - 400) / 2,
     }).render(true);
   }
@@ -132,7 +149,7 @@ export class OseActorSheetCharacter extends OseActorSheet {
     return data;
   }
 
-  async _chooseLang() {
+  async _chooseLang(): Promise<{ choice: string }> {
     let choices = CONFIG.OSE.languages;
 
     let templateData = { choices: choices },
@@ -150,8 +167,9 @@ export class OseActorSheetCharacter extends OseActorSheet {
             label: game.i18n.localize("OSE.Ok"),
             icon: '<i class="fas fa-check"></i>',
             callback: (html) => {
+              debugger;
               resolve({
-                choice: html.find('select[name="choice"]').val(),
+                choice: $(html).find('select[name="choice"]').val() as string,
               });
             },
           },
@@ -165,10 +183,13 @@ export class OseActorSheetCharacter extends OseActorSheet {
     });
   }
 
-  _pushLang(table) {
+  _pushLang(table: any) {
     const data = this.actor.data.data;
+    // @ts-ignore
     let update = duplicate(data[table]);
     this._chooseLang().then((dialogInput) => {
+      debugger;
+      // @ts-ignore choice is a stringified number used as an index on the languages array
       const name = CONFIG.OSE.languages[dialogInput.choice];
       if (update.value) {
         update.value.push(name);
@@ -177,61 +198,69 @@ export class OseActorSheetCharacter extends OseActorSheet {
       }
 
       let newData = {};
+      // @ts-ignore
       newData[table] = update;
       return this.actor.update({ data: newData });
     });
   }
 
-  _popLang(table, lang) {
+  _popLang(table: any, lang: string) {
     const data = this.actor.data.data;
+    // @ts-ignore
     let update = data[table].value.filter((el) => el != lang);
     let newData = {};
+    // @ts-ignore
     newData[table] = { value: update };
     return this.actor.update({ data: newData });
   }
 
   /* -------------------------------------------- */
 
-  _onShowModifiers(event) {
+  _onShowModifiers(event: JQuery.Event) {
     event.preventDefault();
     new OseCharacterModifiers(this.actor, {
+      // @ts-ignore
       top: this.position.top + 40,
+      // @ts-ignore
       left: this.position.left + (this.position.width - 400) / 2,
     }).render(true);
   }
 
-  async _onShowGpCost(event, preparedData) {
+  async _onShowGpCost(event: JQuery.Event, preparedData: any) {
     event.preventDefault();
     new OseCharacterGpCost(this.actor, preparedData, {
+      // @ts-ignore
       top: this.position.top + 40,
+      // @ts-ignore
       left: this.position.left + (this.position.width - 400) / 2,
     }).render(true);
   }
 
-  async _onShowItemTooltip(event) {
+  async _onShowItemTooltip() {
     let templateData = {},
       dlg = await renderTemplate(
         `${OSE.systemPath()}/templates/actors/partials/character-item-tooltip.html`,
         templateData
       );
-    document.querySelector(".game").append(dlg);
+    document.querySelector(".game")?.append(dlg);
   }
 
   /**
    * Activate event listeners using the prepared sheet HTML
    * @param html {HTML}   The prepared HTML object ready to be rendered into the DOM
    */
-  activateListeners(html) {
+  activateListeners(html: JQuery) {
     super.activateListeners(html);
 
     html.find(".ability-score .attribute-name a").click((ev) => {
       let actorObject = this.actor;
       let element = ev.currentTarget;
-      let score = element.parentElement.parentElement.dataset.score;
-      let stat = element.parentElement.parentElement.dataset.stat;
+      let score = element.parentElement?.parentElement?.dataset
+        .score as Attribute;
+      let stat = element.parentElement?.parentElement?.dataset.stat;
       if (!score) {
         if (stat == "lr") {
-          actorObject.rollLoyalty(score, { event: ev });
+          actorObject.rollLoyalty({ event: ev });
         }
       } else {
         actorObject.rollCheck(score, { event: ev });
@@ -241,7 +270,8 @@ export class OseActorSheetCharacter extends OseActorSheet {
     html.find(".exploration .attribute-name a").click((ev) => {
       let actorObject = this.actor;
       let element = ev.currentTarget;
-      let expl = element.parentElement.parentElement.dataset.exploration;
+      let expl = element.parentElement?.parentElement?.dataset
+        .exploration as ExplorationSkill;
       actorObject.rollExploration(expl, { event: ev });
     });
 
@@ -260,6 +290,7 @@ export class OseActorSheetCharacter extends OseActorSheet {
     html.find(".item-push").click((ev) => {
       ev.preventDefault();
       const header = ev.currentTarget;
+      // FIXME: only data-array="..." data-array="language" in character-notes-tab. Perhaps that could be removed?
       const table = header.dataset.array;
       this._pushLang(table);
     });
@@ -275,15 +306,17 @@ export class OseActorSheetCharacter extends OseActorSheet {
     html.find(".item-toggle").click(async (ev) => {
       const li = $(ev.currentTarget).parents(".item");
       const item = this.actor.items.get(li.data("itemId"));
-      await item.update({
+      await item?.update({
         data: {
+          // @ts-ignore need to do some item.data.type checking here?
           equipped: !item.data.data.equipped,
         },
       });
     });
 
     html.find("a[data-action='generate-scores']").click((ev) => {
-      this.generateScores(ev);
+      ev.preventDefault();
+      this.generateScores();
     });
   }
 }
