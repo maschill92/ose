496094b292a58969d9bccb2a5069639eae79c2c1
Michael Schilling <maschill92@gmail.com>
2022-08-19 17:50:32 -0500

    Conver OseCharacterModifiers to an Application

--- src/module/dialog/character-modifiers.ts
+++ src/module/dialog/character-modifiers.ts
@@ -1,23 +1,27 @@
 import { OseActor } from "../actor/entity";
 import { OSE } from "../config";
 
-interface OseCharacterCreatorOptions extends FormApplicationOptions {}
-interface OseCharacterCreatorData
-extends FormApplication.Data<OseActor, OseCharacterCreatorOptions> {
-  user: User;
-}
-// TODO: Can this extend Application?
-export class OseCharacterModifiers extends FormApplication<
-  OseCharacterCreatorOptions,
-  OseCharacterCreatorData
-> {
-  static get defaultOptions() {
-    const options = super.defaultOptions;
-    (options.classes = ["ose", "dialog", "modifiers"]),
-      (options.id = "sheet-modifiers");
-    options.template = `${OSE.systemPath()}/templates/actors/dialogs/modifiers-dialog.html`;
-    options.width = 240;
-    return options;
+export class OseCharacterModifiers extends Application {
+  private readonly actor: OseActor;
+
+  constructor(
+    actor: OseActor,
+    options?: ConstructorParameters<typeof Application>[0]
+  ) {
+    if (!game.user) {
+      throw new Error("Unable to find game.user");
+    }
+    super(options);
+    this.actor = actor;
+  }
+
+  static override get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      id: "sheet-modifiers",
+      classes: ["ose", "dialog", "modifiers"],
+      template: `${OSE.systemPath()}/templates/actors/dialogs/modifiers-dialog.html`,
+      width: 240,
+    });
   }
 
   /* -------------------------------------------- */
@@ -26,9 +30,9 @@ export class OseCharacterModifiers extends FormApplication<
    * Add the Entity name into the window title
    * @type {String}
    */
-  get title() {
+  override get title() {
     // @ts-ignore
-    return `${this.object.name}: Modifiers`;
+    return `${this.actor.name}: Modifiers`;
   }
 
   /* -------------------------------------------- */
@@ -36,26 +40,7 @@ export class OseCharacterModifiers extends FormApplication<
   /**
    * Construct and return the data object used to render the HTML template for this form application.
    */
-  getData(): OseCharacterCreatorData {
-    const data = foundry.utils.deepClone(this.object.data);
-    // TODO: This isn't used in the HTML file anywhere. Remove?
-    //@ts-ignore
-    data.user = game.user;
-    //@ts-ignore
-    return data;
-  }
-
-  /* -------------------------------------------- */
-
-  /** @override */
-  activateListeners(html: JQuery) {
-    super.activateListeners(html);
-  }
-
-  /**
-   * Overriding as it's an abstract function on FormApplication. Perhaps this class should extend Application instead?
-   */
-  override _updateObject(): Promise<unknown> {
-    return Promise.resolve();
+  override getData(): OseActor["data"] {
+    return this.actor.data;
   }
 }
